ecdb41095dbca090045e625f052a847d61d03a62
Daniel Ternyak <dternyak@gmail.com>
2015-09-20 15:25:32 -0500

    blog url resolves post data before load

--- static/app/scripts/app.js
+++ static/app/scripts/app.js
@@ -18,128 +18,142 @@ angular
         'ngRoute',
         'ngTouch',
     ])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'app/views/home.html',
 
-            })
-            .when('/portfolio', {
-                templateUrl: 'app/views/portfolio.html',
+function posts($http, $q) {
+    var deferred = $q.defer();
+    var responsePromise = $http.get("/make_post");
+    responsePromise.success(function (data, status, headers, config) {
+        deferred.resolve(data);
 
-            })
-            .when('/blog', {
-                templateUrl: 'app/views/main.html',
-                controller: 'MainCtrl',
-
-
-            })
-
-            .when('/posts/:param1', {
-                templateUrl: 'app/views/post.html',
-                controller: 'PostCtrl',
-
-
-            })
-            .when('/admin', {
-                templateUrl: 'app/views/protected.html',
-                controller: 'ProtectedCtrl',
-                resolve: {
-                    //This function is injected with the AuthService where you'll put your authentication logic
-                    'auth': function (AuthService, $location, $rootScope) {
-                        return AuthService.isLoggedIn()
-                            .then(function () {
-                                $rootScope.userIsAuthenticated = true;
-                                console.log("continue")
-                            })
-                            .catch(function () {
-                                $location.path('/');
-
-                            });
-                    }
-                }
-
-            })
-            .when('/your_posts', {
-                templateUrl: 'app/views/your_posts.html',
-                controller: 'YourPostsCtrl',
-                resolve: {
-                    //This function is injected with the AuthService where you'll put your authentication logic
-                    'auth': function (AuthService, $location, $rootScope) {
-                        return AuthService.isLoggedIn()
-                            .then(function () {
-                                $rootScope.userIsAuthenticated = true;
-                                console.log("continue")
-                            })
-                            .catch(function () {
-                                $location.path('/');
-
-                            });
-                    }
-                }
-
-            })
-            .when('/your_posts', {
-                templateUrl: 'app/views/your_posts.html',
-                controller: 'YourPostsCtrl',
-                resolve: {
-                    //This function is injected with the AuthService where you'll put your authentication logic
-                    'auth': function (AuthService, $location, $rootScope) {
-                        return AuthService.isLoggedIn()
-                            .then(function () {
-                                $rootScope.userIsAuthenticated = true;
-                                console.log("continue")
-                            })
-                            .catch(function () {
-                                $location.path('/');
-
-                            });
-                    }
-                }
-
-            })
-            .when('/your_posts/:param1', {
-                templateUrl: 'app/views/your_posts_update.html',
-                controller: 'YourPostsUpdateCtrl',
-                resolve: {
-                    //This function is injected with the AuthService where you'll put your authentication logic
-                    'auth': function (AuthService, $location, $rootScope) {
-                        return AuthService.isLoggedIn()
-                            .then(function () {
-                                $rootScope.userIsAuthenticated = true;
-                                console.log("continue")
-                            })
-                            .catch(function () {
-                                $location.path('/');
-
-                            });
-                    }
-                }
-
-            })
-
-            .when('/logout', {
-              templateUrl: 'app/views/logout.html',
-              controller: 'LogoutCtrl',
-                resolve: {
-                    //This function is injected with the AuthService where you'll put your authentication logic
-                    'auth': function (AuthService, $location, $rootScope) {
-                        return AuthService.isLoggedIn()
-                            .then(function () {
-                                $rootScope.userIsAuthenticated = true;
-                                console.log("continuing")
-
-                            })
-                            .catch(function () {
-                                console.log("cach")
-                                $location.path('/login');
-                            });
-                    }
-                }
-            })
-
-            .otherwise({
-                redirectTo: '/'
-            });
     })
+    return deferred.promise;
+
+}
+
+
+angular.module('yoApp').config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'app/views/home.html',
+
+        })
+        .when('/portfolio', {
+            templateUrl: 'app/views/portfolio.html',
+
+        })
+        .when('/blog', {
+            templateUrl: 'app/views/main.html',
+            controller: 'MainCtrl',
+            resolve: {
+                "posts": posts
+            }
+        })
+
+        .when('/posts/:param1', {
+            templateUrl: 'app/views/post.html',
+            controller: 'PostCtrl',
+
+
+        })
+        .when('/admin', {
+            templateUrl: 'app/views/protected.html',
+            controller: 'ProtectedCtrl',
+            resolve: {
+                //This function is injected with the AuthService where you'll put your authentication logic
+                'auth': function (AuthService, $location, $rootScope) {
+                    return AuthService.isLoggedIn()
+                        .then(function () {
+                            $rootScope.userIsAuthenticated = true;
+                            console.log("continue")
+                        })
+                        .catch(function () {
+                            $location.path('/');
+
+                        });
+                }
+            }
+
+        })
+        .when('/your_posts', {
+            templateUrl: 'app/views/your_posts.html',
+            controller: 'YourPostsCtrl',
+            resolve: {
+                //This function is injected with the AuthService where you'll put your authentication logic
+                'auth': function (AuthService, $location, $rootScope) {
+                    return AuthService.isLoggedIn()
+                        .then(function () {
+                            $rootScope.userIsAuthenticated = true;
+                            console.log("continue")
+                        })
+                        .catch(function () {
+                            $location.path('/');
+
+                        });
+                }
+            }
+
+        })
+        .when('/your_posts', {
+            templateUrl: 'app/views/your_posts.html',
+            controller: 'YourPostsCtrl',
+            resolve: {
+                //This function is injected with the AuthService where you'll put your authentication logic
+                'auth': function (AuthService, $location, $rootScope) {
+                    return AuthService.isLoggedIn()
+                        .then(function () {
+                            $rootScope.userIsAuthenticated = true;
+                            console.log("continue")
+                        })
+                        .catch(function () {
+                            $location.path('/');
+
+                        });
+                }
+            }
+
+        })
+        .when('/your_posts/:param1', {
+            templateUrl: 'app/views/your_posts_update.html',
+            controller: 'YourPostsUpdateCtrl',
+            resolve: {
+                //This function is injected with the AuthService where you'll put your authentication logic
+                'auth': function (AuthService, $location, $rootScope) {
+                    return AuthService.isLoggedIn()
+                        .then(function () {
+                            $rootScope.userIsAuthenticated = true;
+                            console.log("continue")
+                        })
+                        .catch(function () {
+                            $location.path('/');
+
+                        });
+                }
+            }
+
+        })
+
+        .when('/logout', {
+            templateUrl: 'app/views/logout.html',
+            controller: 'LogoutCtrl',
+            resolve: {
+                //This function is injected with the AuthService where you'll put your authentication logic
+                'auth': function (AuthService, $location, $rootScope) {
+                    return AuthService.isLoggedIn()
+                        .then(function () {
+                            $rootScope.userIsAuthenticated = true;
+                            console.log("continuing")
+
+                        })
+                        .catch(function () {
+                            console.log("cach")
+                            $location.path('/login');
+                        });
+                }
+            }
+        })
+
+        .otherwise({
+            redirectTo: '/'
+        });
+})
 
--- static/app/scripts/controllers/main.js
+++ static/app/scripts/controllers/main.js
@@ -8,13 +8,7 @@
  * Controller of the yoApp
  */
 angular.module('yoApp')
-    .controller('MainCtrl', function ($scope, $http) {
-        $scope.posts = '';
-
-        var responsePromise = $http.get("/make_post");
-        responsePromise.success(function (data, status, headers, config) {
-            console.log(data)
-            $scope.posts = data
-        })
+    .controller('MainCtrl', function ($scope, $http, posts) {
+        $scope.posts = posts
 
     });
