64f5f5b9af5b27cd7dcde596330a3f729878ba3c
Daniel Ternyak <dternyak@gmail.com>
2015-12-25 19:16:54 -0600

    removed unused boilerplate

--- static/src/routes.js
+++ static/src/routes.js
@@ -4,8 +4,6 @@ import { Route } from 'react-router';
 /* containers */
 import { App } from 'containers/App';
 import { Home } from 'containers/Home';
-import { List } from 'containers/List';
-import {CounterView} from 'containers/Counter'
 import {PortfolioView} from 'containers/PortfolioView'
 import {BlogView} from 'containers/BlogView'
 import {EachPostView} from 'containers/EachPostView'
@@ -13,8 +11,6 @@ import {EachPostView} from 'containers/EachPostView'
 export default (
     <Route path="/" component={App}>
         <Route path="home" component={Home}/>
-        <Route path="list" component={List}/>
-        <Route path="counter" component={CounterView}/>
         <Route path="portfolio" component={PortfolioView}/>
         <Route path="blog" component={BlogView}/>
         <Route path="post/:id" component={EachPostView}/>
